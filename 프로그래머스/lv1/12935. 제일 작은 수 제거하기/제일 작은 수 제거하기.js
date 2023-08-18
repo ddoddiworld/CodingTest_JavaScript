8e4dbcdbd57219d61d178166976c87b681cd104a
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-08-18 20:59:42 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    // 가장 작은 수 제거
+    // 가장 작은 수 찾아서 제거
+    //  빈 배열이면 -1 리턴
+    let newArr = Math.min(...arr); // 가장 작은 값 찾기
+    let newArr2 = arr.filter((item) => item !== newArr);
+
+    if (!newArr2.length == []) {
+        return newArr2;
+    } else {
+        return [-1];
+    }
+}
