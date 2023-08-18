aa6f7b06dddc32eb5f122b383b07fae5be6c5bfa
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-08-18 10:41:27 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    // sqrt 는 특정숫자의 제곱근 값을 계산해주는 함수입니다.
+    // n이 어떤 양의 정수 x의 제곱인지 판단 하고 싶음
+    // n이 제곱이 맞으면 x+1의 제곱을 리턴
+    // n이 제곱이 아니면 -1을 리턴
+
+    if (Math.sqrt(n) % 1 == 0){ // 제곱근 맞음
+        var a = Math.sqrt(n) + 1;
+        return a * a;
+
+    } else { // 제곱근 아님
+        return -1;
+    }
+}
