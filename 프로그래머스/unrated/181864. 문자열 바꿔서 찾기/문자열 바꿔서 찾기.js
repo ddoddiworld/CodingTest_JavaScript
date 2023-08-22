b902df48c9cfc4414203f83331e4e946db0aeb0c
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-08-22 21:21:32 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -0,0 +1,13 @@
+function solution(myString, pat) {
+    var arr = [...myString];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == "A") {
+            arr[i] = "B";
+        } else if (arr[i] == "B") {
+            arr[i] = "A";
+        }
+    }
+
+    return arr.join("").includes(pat) == true ? 1 : 0;
+}
