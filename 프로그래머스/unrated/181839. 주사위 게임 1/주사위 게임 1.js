2e9a7459ebbfacaa30b59c5b368f153077b8dc9e
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-09-02 09:32:00 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,7 +1,7 @@
 function solution(a, b) {
   if (a % 2 != 0 && b % 2 != 0) {
     // a와 b 모두 홀수일때
-    return a * a + b * b;
+    return Math.pow(a, 2) + Math.pow(b, 2);
   }
 
   if (a % 2 == 0 && b % 2 == 0) {
