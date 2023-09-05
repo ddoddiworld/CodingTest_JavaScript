34b476ed3334f14201a96f03cd7d89584f4fd441
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-09-05 09:33:23 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,9 +1,3 @@
 function solution(str_list, ex) {
-  var str = "";
-  for (var i = 0; i < str_list.length; i++) {
-    if (str_list[i].includes(ex) == false) {
-      str += str_list[i];
-    }
-  }
-  return str;
+  return (str_list.filter((item) => !item.includes(ex)).join(""));
 }
