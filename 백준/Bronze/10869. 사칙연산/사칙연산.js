1cc1e337189b4d4ff357ef4ca093bbed44ce611d
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-09-08 09:33:00 +0900

    [Bronze V] Title: 사칙연산, Time: 144 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,9 @@
+var fs = require("fs");
+var input = fs.readFileSync("/dev/stdin").toString().split(" ");
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
