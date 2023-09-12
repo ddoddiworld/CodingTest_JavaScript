1d9a916b62d63c826e2af7163e95d15070a7be20
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-09-12 09:28:01 +0900

    [unrated] Title: 할 일 목록, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,7 @@
+function solution(todo_list, finished) {
+  var answer = [];
+  for (let i = 0; i < todo_list.length; i++) {
+    if (finished[i] == false) answer.push(todo_list[i]);
+  }
+  return answer;
+}
